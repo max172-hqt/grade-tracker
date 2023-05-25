487fb3a9a62c48f9c5f65643eb6992b0f9ad2849
Huy Tran <huytran172@gmail.com>
2023-05-25 18:06:29 -0400

    feat: Set up tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './pages/Home';
+import SettingsScreen from './pages/Setting';
+
+const Tab = createBottomTabNavigator();
 
 function App() {
-  console.log('hello world');
   return (
-    <View style={styles.container}>
-      <Text>Grade Tracker - Group 8</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 registerRootComponent(App);

25b64de54f424b823bc6268544b7bdb67ffb1a4f
PeiwenZhou <p_zhou@fanshaweonline.ca>
2023-06-01 15:47:52 -0400

    Create an example about CourseDetail component get data base on the redux

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -1,5 +1,37 @@
-import { Box } from 'native-base';
+import { Box, Heading, FlatList, HStack, VStack, Text } from 'native-base';
+import { useSelector } from 'react-redux';
+import { selectGradesForCourseWithId } from '../redux/courseSlice';
+import { CourseState } from '../redux/courseSlice';
+import { RouteProp } from '@react-navigation/native';
 
-export default function CourseDetail() {
-  return <Box>Course Detail</Box>;
+type CourseDetailRouteProp = RouteProp<Record<string, { courseId: number }>, 'Course Detail'>;
+
+type Props = {
+  route: CourseDetailRouteProp;
+};
+
+export default function CourseDetail({ route }: Props) {
+  const { courseId } = route.params;
+  const course = useSelector((state) => selectGradesForCourseWithId(state, courseId));
+  console.log(course);
+  return (
+    <Box p="4">
+      <VStack space="4">
+        {course.map((grade) => (
+          <Box key={grade.id} bg="white" p="4" borderRadius="md" shadow={2}>
+            <HStack justifyContent="space-between">
+              <HStack>
+                <Box fontWeight="bold">{grade.data.name}</Box>
+                <Text> : </Text>
+                <Box>{grade.data.actualScore}</Box>
+              </HStack>
+            </HStack>
+            <Text fontSize="sm" color="gray.600">
+              Weight: {grade.data.weight}%
+            </Text>
+          </Box>
+        ))}
+      </VStack>
+    </Box>
+  );
 }

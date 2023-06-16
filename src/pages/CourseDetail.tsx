1a8409cd5464835b60fdab212130c536d6c90c67
PeiwenZhou <p_zhou@fanshaweonline.ca>
2023-06-16 12:56:25 -0400

    [#8] feat: Added functionality to display current score, remaining score to pass, and letter grade

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack, Text, ScrollView } from 'native-base';
+import { Box, VStack, Text, ScrollView, Progress } from 'native-base';
 import { useSelector } from 'react-redux';
 import { selectCourseWithId, selectGradesForCourseWithId } from '../redux/courseSlice';
 import { RouteProp } from '@react-navigation/native';
@@ -20,6 +20,37 @@ export default function CourseDetail({ route }: Props) {
     return null;
   }
 
+  let totalScore = 0;
+  grades.forEach((grade) => {
+    if (grade.data.actualScore && grade.data.weight) {
+      totalScore += grade.data.actualScore * grade.data.weight * 0.01;
+    }
+  });
+
+  const percentage = (totalScore / 100) * 100;
+  const remainingScore = Math.max(50 - totalScore, 0);
+
+  let letterGrade = '';
+  if (percentage >= 90) {
+    letterGrade = 'A+';
+  } else if (percentage >= 80) {
+    letterGrade = 'A';
+  } else if (percentage >= 75) {
+    letterGrade = 'B+';
+  } else if (percentage >= 70) {
+    letterGrade = 'B';
+  } else if (percentage >= 65) {
+    letterGrade = 'C+';
+  } else if (percentage >= 60) {
+    letterGrade = 'C';
+  } else if (percentage >= 55) {
+    letterGrade = 'D+';
+  } else if (percentage >= 50) {
+    letterGrade = 'D';
+  } else {
+    letterGrade = 'F';
+  }
+
   return (
     <Box p={4}>
       <VStack space="2" mb="4">
@@ -34,6 +65,21 @@ export default function CourseDetail({ route }: Props) {
             <CourseGradeItem key={grade.id} grade={grade} />
           ))}
         </VStack>
+        <Box mt={4} mb={20}>
+          <Progress value={percentage} size="lg" colorScheme="teal" />
+          <Text mt={2} fontWeight="bold" textAlign="center">
+            Current Score: {totalScore.toFixed(2)} / 100 ({percentage.toFixed(2)}
+            %)
+          </Text>
+          {remainingScore > 0 && (
+            <Text mt={2} fontWeight="bold" textAlign="center" color="red.500">
+              Remaining Score to Pass: {remainingScore.toFixed(2)}
+            </Text>
+          )}
+          <Text mt={2} fontWeight="bold" textAlign="center">
+            Letter Grade: {letterGrade}
+          </Text>
+        </Box>
       </ScrollView>
     </Box>
   );
